bc41672c5c796a3b3942487eeadfa241ac2953f0
João Lucas Crescencio Granjeiro Barbosa <20221148060002@ifrn.local>
2023-04-13 10:17:02 -0300

    Implementação do exercício

---
+++ aula-edd-01/exercicio.py
@@ -0,0 +1,10 @@
+class Numcomplexo:
+    def __init__(self, real, img) -> None:
+        self.__real = real
+        self.__img = img
+
+    def __str__(self) -> str:
+        if(self.__img >= 0):
+            return f'{self.__real} + {self.__img}'
+        else:
+            return f'{self.__real} {self.__img}'
---
+++ aula-edd-01/teste.py
@@ -0,0 +1,5 @@
+from exercicio import Numcomplexo
+num1 = Numcomplexo(3, 5)
+num2 = Numcomplexo(-7, -4)
+print(num1)
+print(num2)
