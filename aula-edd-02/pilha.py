7d71e21bcbf2d17730dd35de31f66433493d9ddb
João Lucas <joaolucascgbarbosa878@gmail.com>
2023-04-25 11:35:48 -0300

    Criando classe No

--- aula-edd-02/pilha.py
+++ aula-edd-02/pilha.py
@@ -6,11 +6,11 @@ class Pilha:
         return True if len(self.__pilha) == 0 else False
     
     def push(self):
-    
+        pass
     def pop(self):
-    
+        pass
     def peek(self):
-
+        pass
     def list_items(self):
-
+        pass
     def get_size(self):
