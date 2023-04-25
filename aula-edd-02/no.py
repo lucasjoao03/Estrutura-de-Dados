7d4ede0f1cf9a0b8f02a67883ebd48751262de9c
João Lucas <joaolucascgbarbosa878@gmail.com>
2023-04-25 11:51:30 -0300

    Finalização da classe Pilha

--- aula-edd-02/no.py
+++ aula-edd-02/no.py
@@ -2,4 +2,5 @@ class No:
     def __init__(self,valor) -> None:
         self.__valor = valor
         self.__prox = None
-        
+    
+    
--- aula-edd-02/pilha.py
+++ aula-edd-02/pilha.py
@@ -5,12 +5,32 @@ class Pilha:
     def is_empty(self):
         return True if len(self.__pilha) == 0 else False
     
-    def push(self):
-        pass
+    def push(self, valor):
+        self.__pilha.append(valor)
+    
     def pop(self):
-        pass
+        if self.is_empty():
+            raise Exception("Sorry, pilha vazia!!!")
+        else:
+            valor = self.__pilha.pop()
+            return valor
+    
     def peek(self):
-        pass
+        if self.is_empty():
+            raise Exception("Sorry, pilha vazia!!!")
+        else:
+            valor = self.__pilha.pop[-1]
+            return valor
+    
     def list_items(self):
-        pass
+        if self.is_empty():
+            raise Exception("Sorry, pilha vazia!!!")
+        else:
+            print("Relação de itens na Pilha: \n")
+            pilha_invertida =  self.__pilha.reverse()
+            for item in self.__pilha:   
+                print(item)
+
+        
     def get_size(self):
+        return len(self.__pilha)
