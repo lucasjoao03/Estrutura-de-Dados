5e16ff4dd2bad28d55c8b0e0a7a3cb22ff0cde6d
João Lucas <joaolucascgbarbosa878@gmail.com>
2023-04-25 11:33:39 -0300

    Criando uma classe do tipo pilha

---
+++ aula-edd-02/pilha.py
@@ -0,0 +1,16 @@
+class Pilha:
+    def __init__(self) -> None:
+        self.__pilha = []
+    
+    def is_empty(self):
+        return True if len(self.__pilha) == 0 else False
+    
+    def push(self):
+    
+    def pop(self):
+    
+    def peek(self):
+
+    def list_items(self):
+
+    def get_size(self):
